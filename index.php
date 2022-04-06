bf0c219fc5303997340a8bc0a82220c24aee7532
Bastian Allgeier <mail@bastianallgeier.com>
2022-04-06 17:26:25 +0200

    Convert hook to cache driver

--- index.php
+++ index.php
@@ -1,18 +1,11 @@
 <?php
 
-Kirby::plugin('getkirby/staticache', [
-    'hooks' => [
-        'route:after' => function ($result, $path) {
-            // check if it's actually a page request and not some custom route, 
-            // a panel request or anything else.
-            if (is_a($result, 'Kirby\Cms\Page') === true) {
-                // store the file result in DOCUMENT_ROOT/static/some/path/index.html
-                $file = $this->root('index') . '/static/' . ltrim($path . '/index.html', '/');
+load([
+    'Kirby\Cache\StatiCache' => __DIR__ . '/src/StatiCache.php'
+]);
 
-                // add <!-- static --> to the HTML file just so we can use view-source 
-                // to check if it worked. 
-                F::write($file, $result->render() . ' <!-- static -->');
-            }
-        }
+Kirby::plugin('getkirby/staticache', [
+    'cacheTypes' => [
+        'static' => 'Kirby\Cache\StatiCache'
     ]
 ]);
