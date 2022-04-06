5a1d9c6f502281630863f257a78f74ca0813502b
Bastian Allgeier <mail@bastianallgeier.com>
2022-04-06 17:00:19 +0200

    Update the hook code

--- index.php
+++ index.php
@@ -3,9 +3,15 @@
 Kirby::plugin('getkirby/staticache', [
     'hooks' => [
         'route:after' => function ($result, $path) {
+            // check if it's actually a page request and not some custom route, 
+            // a panel request or anything else.
             if (is_a($result, 'Kirby\Cms\Page') === true) {
-                $file = $this->cache('pages')->root() . '/' . ltrim($path . '/index.html', '/');
-                F::write($file, $result->render());
+                // store the file result in DOCUMENT_ROOT/static/some/path/index.html
+                $file = $this->root('index') . '/static/' . ltrim($path . '/index.html', '/');
+
+                // add <!-- static --> to the HTML file just so we can use view-source 
+                // to check if it worked. 
+                F::write($file, $result->render() . ' <!-- static -->');
             }
         }
     ]
